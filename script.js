37c967768a21ef63890319f1443bf473ae978aa0
ORNAGE ABSTRAKT <divjeet.asb@gmail.com>
2022-01-07 00:06:51 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+document.getElementById("dribbble").addEventListener("click", (e) => {
+    e.preventDefault()
+    window.location.href = "https://dribbble.com/InayatBeri"
+})
+
+document.getElementById("behance").addEventListener("click", (e) => {
+    e.preventDefault()
+    window.location.href = "https://www.behance.net/inayatsinghberi"
+})
+
+document.getElementById("insta").addEventListener("click", (e) => {
+    e.preventDefault()
+    window.location.href = "https://www.instagram.com/inayatberi/"
+})
+
+document.getElementById("pinterest").addEventListener("click", (e) => {
+    e.preventDefault()
+    window.location.href = "https://in.pinterest.com/InayatBeri03/_created/"
+})
